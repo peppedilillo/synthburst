0dc1f168817d470a21e33eeb5e0d64bacf162fbf
peppedilillo <peppedilillo@gmail.com>
2023-06-05 14:23:39 +0200

    minors

--- download_tte.py
+++ download_tte.py
@@ -6,7 +6,7 @@ from typing import Dict, List
 import requests
 
 import paths
-from database import get_db, triggered_detectors
+from database import triggered_detectors
 from errors import TTEDownloadError
 
 
